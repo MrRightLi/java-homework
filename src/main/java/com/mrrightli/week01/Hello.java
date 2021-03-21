052e21c67856bbb2ebd7a92dfa4ed2f321104ec7
MrRightli <874519894@qq.com>
2021-03-21 17:35:21 +0800

    自定义ClassLoader

--- src/main/java/com/mrrightli/week01/Hello.java
+++ src/main/java/com/mrrightli/week01/Hello.java
@@ -1,4 +1,46 @@
 package com.mrrightli.week01;
 
 public class Hello {
+    static {
+        System.out.println("Hello Class Initialized!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int[] arr1 = {5, 6, 7, 8};
+        String[] arr2 = {"+", "-", "*", "/"};
+        int[] arr3 = {1, 2, 3, 4};
+
+        for (int i = 0; i < 4; i++) {
+            if (arr2[i].equals("+")) {
+                System.out.println(add(arr1[i], arr3[i]));
+            }
+            if (arr2[i].equals("-")) {
+                System.out.println(subtract(arr1[i], arr3[i]));
+            }
+            if (arr2[i].equals("*")) {
+                System.out.println(multiply(arr1[i], arr3[i]));
+            }
+            if (arr2[i].equals("/")) {
+                System.out.println(divide(arr1[i], arr3[i]));
+            }
+        }
+    }
+
+    //
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
 }
---
+++ src/main/java/com/mrrightli/week01/MyClassLoader.java
@@ -0,0 +1,51 @@
+package com.mrrightli.week01;
+
+import java.io.*;
+
+
+public class MyClassLoader extends ClassLoader {
+
+    public static void main(String[] args) throws Exception {
+        new MyClassLoader().findClass("Hello").newInstance();
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = getContent("/Users/mrrightli/Documents/workplace/java/java-homework/src/main/java/com/mrrightli/week01/Hello.xlass");
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - bytes[i]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    public byte[] getContent(String filePath) throws IOException {
+        File file = new File(filePath);
+        long fileSize = file.length();
+        if (fileSize > Integer.MAX_VALUE) {
+            System.out.println("file too big...");
+            return null;
+        }
+        FileInputStream fi = new FileInputStream(file);
+        byte[] buffer = new byte[(int) fileSize];
+        int offset = 0;
+        int numRead = 0;
+        while (offset < buffer.length
+                && (numRead = fi.read(buffer, offset, buffer.length - offset)) >= 0) {
+            offset += numRead;
+        }
+        // 确保所有数据均被读取
+        if (offset != buffer.length) {
+            throw new IOException("Could not completely read file "
+                    + file.getName());
+        }
+        fi.close();
+        return buffer;
+    }
+}
