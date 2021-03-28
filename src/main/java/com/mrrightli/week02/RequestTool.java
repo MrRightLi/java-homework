d1d00070ff11a658c364147184e13895cdc8feb8
MrRightli <874519894@qq.com>
2021-03-28 19:53:10 +0800

    RequestTool

--- src/main/java/com/mrrightli/week02/RequestTool.java
+++ src/main/java/com/mrrightli/week02/RequestTool.java
@@ -9,7 +9,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.*;
